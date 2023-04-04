774ff80bb8a52c83245b09a188c7879dc1623276
LucianoGjinaj <73044600+LucianoGjinaj@users.noreply.github.com>
2023-04-04 22:52:28 +0200

    Add files via upload

---
+++ TicTacToe/TicTacToe/Form1.Designer.cs
@@ -0,0 +1,169 @@
+﻿namespace TicTacToe
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button00 = new System.Windows.Forms.Button();
+            this.button01 = new System.Windows.Forms.Button();
+            this.button02 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.button11 = new System.Windows.Forms.Button();
+            this.button12 = new System.Windows.Forms.Button();
+            this.button20 = new System.Windows.Forms.Button();
+            this.button21 = new System.Windows.Forms.Button();
+            this.button22 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button00
+            // 
+            this.button00.Font = new System.Drawing.Font("Leelawadee", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button00.Location = new System.Drawing.Point(118, 71);
+            this.button00.Name = "button00";
+            this.button00.Size = new System.Drawing.Size(77, 77);
+            this.button00.TabIndex = 0;
+            this.button00.UseVisualStyleBackColor = true;
+            this.button00.Click += new System.EventHandler(this.button00_Click);
+            // 
+            // button01
+            // 
+            this.button01.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.button01.Font = new System.Drawing.Font("Leelawadee", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button01.Location = new System.Drawing.Point(201, 71);
+            this.button01.Name = "button01";
+            this.button01.Size = new System.Drawing.Size(77, 77);
+            this.button01.TabIndex = 1;
+            this.button01.UseVisualStyleBackColor = true;
+            this.button01.Click += new System.EventHandler(this.button01_Click);
+            // 
+            // button02
+            // 
+            this.button02.Font = new System.Drawing.Font("Leelawadee", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button02.Location = new System.Drawing.Point(284, 71);
+            this.button02.Name = "button02";
+            this.button02.Size = new System.Drawing.Size(77, 77);
+            this.button02.TabIndex = 2;
+            this.button02.UseVisualStyleBackColor = true;
+            this.button02.Click += new System.EventHandler(this.button02_Click);
+            // 
+            // button10
+            // 
+            this.button10.Font = new System.Drawing.Font("Leelawadee", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button10.Location = new System.Drawing.Point(118, 154);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(77, 77);
+            this.button10.TabIndex = 3;
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.button10_Click);
+            // 
+            // button11
+            // 
+            this.button11.Font = new System.Drawing.Font("Leelawadee", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button11.Location = new System.Drawing.Point(201, 154);
+            this.button11.Name = "button11";
+            this.button11.Size = new System.Drawing.Size(77, 77);
+            this.button11.TabIndex = 4;
+            this.button11.UseVisualStyleBackColor = true;
+            this.button11.Click += new System.EventHandler(this.button11_Click);
+            // 
+            // button12
+            // 
+            this.button12.Font = new System.Drawing.Font("Leelawadee", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button12.Location = new System.Drawing.Point(284, 154);
+            this.button12.Name = "button12";
+            this.button12.Size = new System.Drawing.Size(77, 77);
+            this.button12.TabIndex = 5;
+            this.button12.UseVisualStyleBackColor = true;
+            this.button12.Click += new System.EventHandler(this.button12_Click);
+            // 
+            // button20
+            // 
+            this.button20.Font = new System.Drawing.Font("Leelawadee", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button20.Location = new System.Drawing.Point(118, 237);
+            this.button20.Name = "button20";
+            this.button20.Size = new System.Drawing.Size(77, 77);
+            this.button20.TabIndex = 6;
+            this.button20.UseVisualStyleBackColor = true;
+            this.button20.Click += new System.EventHandler(this.button20_Click);
+            // 
+            // button21
+            // 
+            this.button21.Font = new System.Drawing.Font("Leelawadee", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button21.Location = new System.Drawing.Point(201, 237);
+            this.button21.Name = "button21";
+            this.button21.Size = new System.Drawing.Size(77, 77);
+            this.button21.TabIndex = 7;
+            this.button21.UseVisualStyleBackColor = true;
+            this.button21.Click += new System.EventHandler(this.button21_Click);
+            // 
+            // button22
+            // 
+            this.button22.Font = new System.Drawing.Font("Leelawadee", 26.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button22.Location = new System.Drawing.Point(284, 237);
+            this.button22.Name = "button22";
+            this.button22.Size = new System.Drawing.Size(77, 77);
+            this.button22.TabIndex = 8;
+            this.button22.UseVisualStyleBackColor = true;
+            this.button22.Click += new System.EventHandler(this.button22_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(484, 461);
+            this.Controls.Add(this.button22);
+            this.Controls.Add(this.button21);
+            this.Controls.Add(this.button20);
+            this.Controls.Add(this.button12);
+            this.Controls.Add(this.button11);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button02);
+            this.Controls.Add(this.button01);
+            this.Controls.Add(this.button00);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Tic Tac Toe";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button00;
+        private System.Windows.Forms.Button button01;
+        private System.Windows.Forms.Button button02;
+        private System.Windows.Forms.Button button10;
+        private System.Windows.Forms.Button button11;
+        private System.Windows.Forms.Button button12;
+        private System.Windows.Forms.Button button20;
+        private System.Windows.Forms.Button button21;
+        private System.Windows.Forms.Button button22;
+    }
+}
+
---
+++ TicTacToe/TicTacToe/Form1.cs
@@ -0,0 +1,193 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TicTacToe
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        string[] gameBoard = new string[9];
+        int currentTurn = 0;
+
+        public string returnSymbol(int turn)
+        {
+            if(turn % 2 == 0)
+            {
+                return "O";
+            }
+            else
+            {
+                return "X";
+            }
+        }
+
+        public void checkWinner()
+        {
+            for (int i=0; i<9; i++)
+            {
+                string combination = "";
+
+                switch (i)
+                {
+                    case 0:
+                        combination = gameBoard[0] + gameBoard[4] + gameBoard[8];
+                        break;
+                    case 1:
+                        combination = gameBoard[2] + gameBoard[4] + gameBoard[6];
+                        break;
+                    case 2:
+                        combination = gameBoard[0] + gameBoard[1] + gameBoard[2];
+                        break;
+                    case 3:
+                        combination = gameBoard[3] + gameBoard[4] + gameBoard[5];
+                        break;
+                    case 4:
+                        combination = gameBoard[6] + gameBoard[7] + gameBoard[8];
+                        break;
+                    case 5:
+                        combination = gameBoard[0] + gameBoard[3] + gameBoard[6];
+                        break;
+                    case 6:
+                        combination = gameBoard[1] + gameBoard[4] + gameBoard[7];
+                        break;
+                    case 7:
+                        combination = gameBoard[2] + gameBoard[5] + gameBoard[8];
+                        break;
+                }
+
+                if (combination.Equals("OOO"))
+                {
+                    MessageBox.Show("Playear O wins the game","We have a winner", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    reset();
+                    return;
+                }else if (combination.Equals("XXX"))
+                {
+                    MessageBox.Show("Playear X wins the game", "We have a winner", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    reset();
+                    return;
+                }
+            }
+            checkDraw();
+        }
+
+        public void reset()
+        {
+            button00.Text = "";
+            button01.Text = "";
+            button02.Text = "";
+            button10.Text = "";
+            button11.Text = "";
+            button12.Text = "";
+            button20.Text = "";
+            button21.Text = "";
+            button22.Text = "";
+            gameBoard = new string[9];
+            currentTurn = 0;
+
+        }
+
+        public void checkDraw()
+        {
+            int counter = 0;
+            for(int i = 0; i < gameBoard.Length; i++)
+            {
+                if(gameBoard[i] != null) { counter++; }
+                if(counter == 9)
+                {
+                    MessageBox.Show("That is a Draw","We don't have a winner", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                    reset();
+                }
+            }
+        }
+        private void button00_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[0] = returnSymbol(currentTurn);
+            button00.Text = gameBoard[0];
+            checkWinner();
+        }
+
+        private void button01_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[1] = returnSymbol(currentTurn);
+            button01.Text = gameBoard[1];
+            checkWinner();
+
+        }
+
+        private void button02_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[2] = returnSymbol(currentTurn);
+            button02.Text = gameBoard[2];
+            checkWinner();
+
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[3] = returnSymbol(currentTurn);
+            button10.Text = gameBoard[3];
+            checkWinner();
+
+        }
+
+        private void button11_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[4] = returnSymbol(currentTurn);
+            button11.Text = gameBoard[4];
+            checkWinner();
+
+        }
+
+        private void button12_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[5] = returnSymbol(currentTurn);
+            button12.Text = gameBoard[5];
+            checkWinner();
+
+        }
+
+        private void button20_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[6] = returnSymbol(currentTurn);
+            button20.Text = gameBoard[6];
+            checkWinner();
+
+        }
+
+        private void button21_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[7] = returnSymbol(currentTurn);
+            button21.Text = gameBoard[7];
+            checkWinner();
+
+        }
+
+        private void button22_Click(object sender, EventArgs e)
+        {
+            currentTurn++;
+            gameBoard[8] = returnSymbol(currentTurn);
+            button22.Text = gameBoard[8];
+            checkWinner();
+
+        }
+    }
+}
